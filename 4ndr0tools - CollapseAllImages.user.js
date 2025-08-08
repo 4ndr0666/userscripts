6a61e9c2e610c7a378b7f56043402b17563d965f
4ndr0666 <01_dolor.loftier@icloud.com>
2025-08-08 02:10:26 -0500

    added new 4ndr0tool to collapse all images and reduce excessive I/O. Just hover to rehydrate

---
+++ 4ndr0tools - CollapseAllImages.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         4ndr0tools - Collapse All Images
+// @namespace    http://github.com/4ndr0666/userscripts
+// @author       4ndr0666
+// @version      1.2.0
+// @description  Minimizes all images on a site and restores then with a mouse hover for reducing excessive I/O.
+// @match        http*://*/*
+// @icon         https://raw.githubusercontent.com/4ndr0666/4ndr0site/refs/heads/main/static/cyanglassarch.png
+// @license      AGPL-v3.0
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let size = "200px";
+
+    GM_addStyle(`
+    img {
+       transition: max-width 0.4s ease-in-out, max-height 0.4s ease-in-out;
+       max-width : ${size};
+       max-height : ${size};
+       object-fit: contain;
+    }
+    img:hover{
+       max-width : 100%;
+       max-height : 100%;
+    }
+    `)
+
+})();
