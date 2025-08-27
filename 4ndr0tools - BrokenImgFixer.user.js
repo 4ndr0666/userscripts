b231ad7f8d413ccb59068cf51f325c60ba28af9a
4ndr0666 <01_dolor.loftier@icloud.com>
2025-08-27 06:37:42 -0500

    Update 4ndr0tools - BrokenImgFixer.user.js

--- 4ndr0tools - BrokenImgFixer.user.js
+++ 4ndr0tools - BrokenImgFixer.user.js
@@ -6,6 +6,7 @@
 // @description Part of 4ndr0tools - Shows and reloads broken images
 // downloadURL  https://github.com/4ndr0666/userscripts/raw/refs/heads/main/4ndr0tools%20-%20BrokenImgFixer.user.js
 // @updateURL   https://github.com/4ndr0666/userscripts/raw/refs/heads/main/4ndr0tools%20-%20BrokenImgFixer.user.js
+// @icon         https://raw.githubusercontent.com/4ndr0666/4ndr0site/refs/heads/main/static/cyanglassarch.png
 // @match       *://*/*
 // @license     MIT
 // @grant       none
