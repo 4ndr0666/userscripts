e4cdfa7f9645aec6706eb52631c7beaad8f26c09
4ndr0666 <01_dolor.loftier@icloud.com>
2025-12-26 00:00:15 -0600

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+// eslint.config.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    greasemonkey: true
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    "no-unused-vars": ["warn"],
+    "no-undef": ["error"],
+    "no-console": ["off"],
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "eqeqeq": ["error", "always"],
+    "curly": ["error", "multi-line"],
+    "no-empty-function": ["warn"]
+  },
+  globals: {
+    GM_xmlhttpRequest: "readonly",
+    GM_download: "readonly",
+    GM_setClipboard: "readonly",
+    GM_setValue: "readonly",
+    GM_getValue: "readonly",
+    GM_log: "readonly",
+    GM_openInTab: "readonly",
+    unsafeWindow: "readonly",
+    JSZip: "readonly",
+    tippy: "readonly",
+    sha256: "readonly",
+    saveAs: "readonly"
+  }
+};
