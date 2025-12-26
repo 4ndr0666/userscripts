0fca25716d6fcd970994e0d8c5838eafd4ff69ca
4ndr0666 <01_dolor.loftier@icloud.com>
2025-12-26 01:15:44 -0600

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -6,39 +6,33 @@ export default {
     es2021: true,
     greasemonkey: true
   },
-  {
-    ...js.configs.recommended,
-    languageOptions: {
-      ...js.configs.recommended.languageOptions,
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.es2021,
-        GM_xmlhttpRequest: "readonly",
-        GM_download: "readonly",
-        GM_setClipboard: "readonly",
-        GM_setValue: "readonly",
-        GM_getValue: "readonly",
-        GM_log: "readonly",
-        GM_openInTab: "readonly",
-        unsafeWindow: "readonly",
-        JSZip: "readonly",
-        tippy: "readonly",
-        sha256: "readonly",
-        saveAs: "readonly",
-        m3u8Parser: "readonly",
-      },
-    },
-    rules: {
-      "no-unused-vars": ["warn"],
-      "no-undef": ["error"],
-      "no-console": ["off"],
-      "semi": ["error", "always"],
-      "quotes": "off",
-      "eqeqeq": ["error", "always"],
-      "curly": ["warn", "multi-line"],
-      "no-empty-function": ["warn"],
-    },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-];
+  rules: {
+    "no-unused-vars": ["warn"],
+    "no-undef": ["error"],
+    "no-console": ["off"],
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "eqeqeq": ["error", "always"],
+    "curly": ["error", "multi-line"],
+    "no-empty-function": ["warn"]
+  },
+  globals: {
+    GM_xmlhttpRequest: "readonly",
+    GM_download: "readonly",
+    GM_setClipboard: "readonly",
+    GM_setValue: "readonly",
+    GM_getValue: "readonly",
+    GM_log: "readonly",
+    GM_openInTab: "readonly",
+    unsafeWindow: "readonly",
+    JSZip: "readonly",
+    tippy: "readonly",
+    sha256: "readonly",
+    saveAs: "readonly"
+  }
+};
