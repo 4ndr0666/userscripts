517017ef233556b1e59d33579a175a366413968f
4ndr0666 <01_dolor.loftier@icloud.com>
2026-08-23 15:39:49 -0500

    Rename script from 'YouTube_Filter' to 'YT Filter'
    Signed-off-by: 4ndr0666 <01_dolor.loftier@icloud.com>

--- 4ndr0tools - YT Filter.user.js
+++ 4ndr0tools - YT Filter.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         4ndr0tools - YouTube_Filter
+// @name         4ndr0tools - YT Filter
 // @namespace    https://github.com/4ndr0666/userscripts
 // @version      2.4
 // @author       4ndr0666
