b0d4d67b7d0b2419c693b98c277a98491466991a
4ndr0666 <01_dolor.loftier@icloud.com>
2026-09-19 00:38:00 -0400

    Rename user script from PromptMasterBeta to PromptMaster
    Signed-off-by: 4ndr0666 <01_dolor.loftier@icloud.com>

--- 4ndr0tools - Prompt Master.user.js
+++ 4ndr0tools - Prompt Master.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name                PromptMasterBeta
+// @name                PromptMaster
 // @namespace           https://github.com/4ndr0666/userscripts
 // @version             27.3.17
 // @author              4ndr0666
