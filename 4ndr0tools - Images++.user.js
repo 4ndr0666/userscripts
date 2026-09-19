bfd08464130a9d31dad0d41f47ff7628c1faddaa
4ndr0666 <01_dolor.loftier@icloud.com>
2026-09-19 01:15:19 -0400

    Modify user script metadata and license
    Updated metadata for the user script, including license and icon format.
    
    Signed-off-by: 4ndr0666 <01_dolor.loftier@icloud.com>

--- 4ndr0tools - Images++.user.js
+++ 4ndr0tools - Images++.user.js
@@ -1,55 +1,51 @@
 // ==UserScript==
-// @name 4ndr0tools - Images++
-// @namespace https://github.com/4ndr0666/userscripts
-// @version 3.0.0
-// @description Shows images/videos behind links via mouseover, with an integrated mode to collapse all page images for performance.
-// @author 4ndr0666
-// @match  *://*/*
-// @run-at document-start
-//
-// @grant GM_addElement
-// @grant GM_addStyle
-// @grant GM_download
-// @grant GM_getValue
-// @grant GM_getValues
-// @grant GM_openInTab
-// @grant GM_registerMenuCommand
-// @grant GM_unregisterMenuCommand
-// @grant GM_setClipboard
-// @grant GM_setValue
-// @grant GM_xmlhttpRequest
-//
-// @grant GM.getValue
-// @grant GM.openInTab
-// @grant GM.registerMenuCommand
-// @grant GM.unregisterMenuCommand
-// @grant GM.setClipboard
-// @grant GM.setValue
-// @grant GM.xmlHttpRequest
-//
-// @connect *
-// @connect self
-// @connect github.com
-// @connect deviantart.com
-// @connect facebook.com
-// @connect fbcdn.com
-// @connect flickr.com
-// @connect simp6.jp5.su
-// @connect jp5.su
-// @connect jp6.su
-// @connect googleusercontent.com
-// @connect gyazo.com
-// @connect imgur.com
-// @connect instagr.am
-// @connect instagram.com
-// @connect prnt.sc
-// @connect prntscr.com
-// @connect user-images.githubusercontent.com
-//
-// @icon https://raw.githubusercontent.com/4ndr0666/4ndr0site/refs/heads/main/static/cyanglassarch.png
-// @downloadURL https://github.com/4ndr0666/userscripts/raw/refs/heads/main/4ndr0tools%20-%20Images++.user.js
-// @updateURL https://github.com/4ndr0666/userscripts/raw/refs/heads/main/4ndr0tools%20-%20Images++.user.js
-// @license AGPL-v3.0; MIT
+// @name         4ndr0tools - Images++
+// @namespace    https://github.com/4ndr0666/userscripts
+// @version      3.0.0
+// @description  Shows images/videos behind links via mouseover, with an integrated mode to collapse all page images for performance.
+// @author       4ndr0666
+// @match        *://*/*
+// @run-at       document-start
+// @grant        GM_addElement
+// @grant        GM_addStyle
+// @grant        GM_download
+// @grant        GM_getValue
+// @grant        GM_getValues
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        GM_setClipboard
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM.getValue
+// @grant        GM.openInTab
+// @grant        GM.registerMenuCommand
+// @grant        GM.unregisterMenuCommand
+// @grant        GM.setClipboard
+// @grant        GM.setValue
+// @grant        GM.xmlHttpRequest
+// @connect      *
+// @connect      self
+// @connect      github.com
+// @connect      deviantart.com
+// @connect      facebook.com
+// @connect      fbcdn.com
+// @connect      flickr.com
+// @connect      simp6.jp5.su
+// @connect      jp5.su
+// @connect      jp6.su
+// @connect      googleusercontent.com
+// @connect      gyazo.com
+// @connect      imgur.com
+// @connect      instagr.am
+// @connect      instagram.com
+// @connect      prnt.sc
+// @connect      prntscr.com
+// @connect      user-images.githubusercontent.com
+// @icon         data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20128%20128%22%20fill%3D%22none%22%20stroke%3D%22%2300E5FF%22%20stroke-width%3D%223%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3Cpath%20d%3D%22M%2064%2C12%20A%2052%2C52%200%201%201%2063.9%2C12%20Z%22%20stroke-dasharray%3D%2221.78%2021.78%22%20stroke-width%3D%222%22%2F%3E%3Cpath%20d%3D%22M%2064%2C20%20A%2044%2C44%200%201%201%2063.9%2C20%20Z%22%20stroke-dasharray%3D%2210%2010%22%20stroke-width%3D%221.5%22%20opacity%3D%220.7%22%2F%3E%3Cpath%20d%3D%22M64%2030%20L91.3%2047%20L91.3%2081%20L64%2098%20L36.7%2081%20L36.7%2047%20Z%22%2F%3E%3Ctext%20x%3D%2264%22%20y%3D%2267%22%20text-anchor%3D%22middle%22%20dominant-baseline%3D%22middle%22%20fill%3D%22%2300E5FF%22%20stroke%3D%22none%22%20font-size%3D%2256%22%20font-weight%3D%22700%22%20font-family%3D%22Cinzel%20Decorative%2C%20serif%22%3E%CE%A8%3C%2Ftext%3E%3C%2Fsvg%3E
+// @downloadURL  https://github.com/4ndr0666/userscripts/raw/refs/heads/main/4ndr0tools%20-%20Images++.user.js
+// @updateURL    https://github.com/4ndr0666/userscripts/raw/refs/heads/main/4ndr0tools%20-%20Images++.user.js
+// @license      UNLICENSED - RED TEAM USE ONLY
 // ==/UserScript==
 
 'use strict';
